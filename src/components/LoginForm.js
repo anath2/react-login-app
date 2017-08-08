42a8921640b63bab27d8067cf824d084c8c5b005
Ajit Nath <nath.ajit01@gmail.com>
2017-08-08 23:25:57 +0800

    Setting up the readme

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link, withRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { func, object } from 'prop-types'
 
 export default class LoginForm extends Component {
