5c7ce073c95b2b0d64f09fbf58a8235e9d4b9b24
Ajit Nath <nath.ajit01@gmail.com>
2017-08-20 12:02:40 +0800

    FIx - Error messages to be more specific on the Textfield components

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,16 +1,32 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const TextField = ({error, type, name, placeholder}) => { 
-    const messages = {
-        TAKEN: " is already in use",
-        EMPTY: "is empty",
-        INVALID: "is not valid"
+    const message = {
+        email: {
+            TAKEN: "email already in use",
+            EMPTY: "email empty",
+            INVALID: "please enter a vaild email address"
+        },
+        username: {
+            TAKEN: "username already in use",
+            EMPTY: "username empty",
+            INVALID: "username cannot contain spaces",
+        },
+        password1: {            
+            EMPTY: "password empty",
+        }, 
+        password2: {            
+            EMPTY: "password empty",
+            INVALID: "passwords do not match",
+        }
     }
     let errorMessage 
-
+    let errorObject = message[name];
+    console.log("The error object is", errorObject)
     if (error !== undefined) {
-        if (Object.keys(messages).indexOf(error) !== -1 ) {
-            errorMessage = "Field " + messages[error]
+        if (Object.keys(errorObject).indexOf(error) !== -1 ) {
+            errorMessage = errorObject[error]
         }
         else {
             errorMessage = "Error"
@@ -27,5 +43,12 @@ const TextField = ({error, type, name, placeholder}) => {
 )};
 
 // TODO: proptype declarations
+
+TextField.prototype = {
+    error: PropTypes.string,
+    type: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    placeholder: PropTypes.string
+}
 
 export default TextField
