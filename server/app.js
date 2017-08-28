a36a143d0948ff380c58ff684f92977b0a930835
Ajit Nath <nath.ajit01@gmail.com>
2017-08-28 18:14:43 +0800

    Setting up express with react-create-app

--- server/app.js
+++ server/app.js
@@ -6,11 +6,11 @@ const app = express()
 
 // MIDDLEWARE
 app.use(morgan('tiny'))
-app.use(express.static(path.resolve(__dirname, '..', 'public')))
+app.use(express.static(path.resolve(__dirname, '..', 'build')))
 
 // ROUTES - ALWAYS POINT TO THE INDEX FILE
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))
 })
 
 module.exports = app
